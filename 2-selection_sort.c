ed3dd5e7a4071e2027cc718cb625bacb80a17457
quentin <quentin.rio.pro@gmail.com>
2022-08-23 13:59:32 -0700

    V.1 of sort algo (changed how it fundamentaly work)

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,30 +8,26 @@
  */
 
 
-void selection_sort(int *array, size_t size)
-{
-	size_t test = 1, i, j, tmp;
+void selection_sort(int *array, size_t size) {
+	size_t test = 1, index = 0, a_index, i, j = 0;
 
-	if (!(array))
-		exit(EXIT_FAILURE);
-
-	while (test != 0)
+	for(i = 0; i < size - 1; i++)
 	{
-		test = 0;
-		for (i = 0; i < size; i++)
+		index = i;
+		for(j = i + 1; j < size; j++)
 		{
-			for (j = i; j < size; j++)
-			{
-				if (array[i] > array[j])
-				{
-					tmp = array[i];
-					array[i] = array[j];
-					array[j] = tmp;
-					test++;
-					print_array(array, size);
-				}
-			}
+			if (array[index] > array[j])
+				index = j;
 		}
-	}
+		if (index == i)
+			continue;
+		if (index != i)
+		{
+			a_index = array[i];
+			array[i] = array[index];
+			array[index] = a_index;
+		}
+		print_array(array, size);
 
+	}
 }
