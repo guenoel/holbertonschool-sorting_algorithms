8535fe69ecc29ed5c8acd52c9c3a2e0bac8cf5b5
guenoel <guenooel@hotmail.com>
2022-08-23 11:21:56 +0200

    task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - function to sort throught bubble method
+ * @array: array
+ * @size: size of the array
+ *
+ * Return: Nothing
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t n;
+	int i = 1;
+	int tmp;
+
+	if (!(array) || size < 2)
+		return;
+
+	while (i != 0)
+	{
+		i = 0;
+		for (n = 0; n < size; n++)
+		{
+			if (array[n] > array[n + 1])
+			{
+				tmp = array[n];
+				array[n] = array[n + 1];
+				array[n + 1] = tmp;
+				i++;
+				print_array(array, size);
+			}
+		}
+	}
+}
--- print_array.c
+++ print_array.c
@@ -9,17 +9,17 @@
  */
 void print_array(const int *array, size_t size)
 {
-    size_t i;
+	size_t i;
 
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
-    }
-    printf("\n");
+	i = 0;
+	while (array && i < size)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+		++i;
+	}
+	printf("\n");
 }
 
 
@@ -34,16 +34,16 @@ void print_array(const int *array, size_t size)
  */
 void print_list(const listint_t *list)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while (list)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", list->n);
-        ++i;
-        list = list->next;
-    }
-    printf("\n");
+	i = 0;
+	while (list)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", list->n);
+		++i;
+		list = list->next;
+	}
+	printf("\n");
 }
