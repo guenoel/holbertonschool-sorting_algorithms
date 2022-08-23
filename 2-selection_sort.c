71e090739ea2af62903e83aa38a835fd05948c6a
quentin <quentin.rio.pro@gmail.com>
2022-08-23 05:51:40 -0700

    V.0 of sort algo

---
+++ 2-selection_sort.c
@@ -0,0 +1,37 @@
+#include "header.h"
+
+/**
+ * selection_sort - alg to sort in ascending order
+ * @array: array
+ * @size: array size
+ * Return: void
+ */
+
+
+void selection_sort(int *array, size_t size)
+{
+	int test = 1, i, j, tmp;
+
+	if (!(array))
+		exit(EXIT_FAILURE);
+
+	while (test != 0)
+	{
+		test = 0;
+		for (i = 0; i < (size - 1); i++)
+		{
+			for (j = 0; j < (size -1); j++)
+			{
+				if (array[i] > array[j])
+				{
+					tmp = array[i];
+					array[i] = array[j];
+					array[j] = tmp;
+					test++;
+					print_array(array, size);
+				}
+			}
+		}
+	}
+
+}
